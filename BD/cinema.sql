7c75a1aa84c41122fb8e855e16fa5c76f6f94ed9
Eduardo Cavalcante de Brito <edwinclarwis777@gmail.com>
2025-09-03 13:38:26 -0300

    BD

---
+++ BD/cinema.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03/09/2025 às 18:33
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `cinema`
+--
+CREATE DATABASE IF NOT EXISTS `cinema` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cinema`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `ID_cliente` int(11) NOT NULL,
+  `Nome` varchar(50) NOT NULL,
+  `CPF` varchar(14) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Telefone` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `filme`
+--
+
+CREATE TABLE `filme` (
+  `Cod_Filme` int(11) NOT NULL,
+  `Nome` varchar(25) NOT NULL,
+  `Duracao` time NOT NULL,
+  `Classificacao` varchar(5) NOT NULL,
+  `Genero` varchar(40) NOT NULL,
+  `Sinopse` varchar(1024) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ingresso`
+--
+
+CREATE TABLE `ingresso` (
+  `ID_Ing` int(11) NOT NULL,
+  `ID_cliente` int(11) NOT NULL,
+  `Cod_Ses` int(11) NOT NULL,
+  `Preco_ing` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sala`
+--
+
+CREATE TABLE `sala` (
+  `Num_Sala` int(11) NOT NULL,
+  `Num_Assent` int(11) NOT NULL,
+  `Tipo_Sala` varchar(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sessao`
+--
+
+CREATE TABLE `sessao` (
+  `Cod_Ses` int(11) NOT NULL,
+  `Ing_dis` int(11) NOT NULL,
+  `Cod_Filme` int(11) NOT NULL,
+  `Num_Sala` int(11) NOT NULL,
+  `horario` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ID_cliente`);
+
+--
+-- Índices de tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD PRIMARY KEY (`Cod_Filme`);
+
+--
+-- Índices de tabela `ingresso`
+--
+ALTER TABLE `ingresso`
+  ADD PRIMARY KEY (`ID_Ing`);
+
+--
+-- Índices de tabela `sala`
+--
+ALTER TABLE `sala`
+  ADD PRIMARY KEY (`Num_Sala`);
+
+--
+-- Índices de tabela `sessao`
+--
+ALTER TABLE `sessao`
+  ADD PRIMARY KEY (`Cod_Ses`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `ID_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filme`
+--
+ALTER TABLE `filme`
+  MODIFY `Cod_Filme` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `ingresso`
+--
+ALTER TABLE `ingresso`
+  MODIFY `ID_Ing` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `sala`
+--
+ALTER TABLE `sala`
+  MODIFY `Num_Sala` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `sessao`
+--
+ALTER TABLE `sessao`
+  MODIFY `Cod_Ses` int(11) NOT NULL AUTO_INCREMENT;
